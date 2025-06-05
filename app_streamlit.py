d1c46eda95e6cb83ae6dfab02f87cad491c3fc90
CriHerrera <cris.herreraduarte@gmail.com>
2025-06-05 17:16:28 -0400

    saf
    ds

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,8 +3,9 @@ import os
 import pandas as pd
 
 # Paths
-# Usar ruta relativa al directorio actual
-becas = pd.read_csv('becas_procesadas_para_dash.csv', sep=";")
+# Buscar el archivo en el directorio inputs
+input_path = os.path.join(os.path.dirname(__file__), 'inputs', 'becas_procesadas_para_dash.csv')
+becas = pd.read_csv(input_path, sep=";")
 
 # Colapsar por name y numero_corrida
 becas_collapsed = becas.groupby(['name', 'numero_corrida']).first().reset_index()
